b569db3b2867c3045964b6a8d33615c6573374fc
Kaur <fhiakau@frgeneseq03.had.sa.gov.au>
2023-09-20 10:03:09 +0930

    Changed the col number to reference new sum sheet

--- scripts/phet_makefile.sh
+++ scripts/phet_makefile.sh
@@ -3,27 +3,29 @@
 
 
 
-awk 'BEGIN {print "salmonella:"}; (FS="\t") {if($10 == "Salmonella enterica" && $2 >= 1000000 && $16 <= 500 ) print "- " $1 }' QC_summary.txt
+awk 'BEGIN {print "salmonella:"}; (FS="\t") {if($9 == "Salmonella enterica" && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' QC_summary.txt
 
-awk 'BEGIN {print "listeria:"}; (FS="\t") {if($10 ~/Listeria/ && $2 >= 1000000 && $16 <= 500 ) print "- " $1 }' QC_summary.txt
+awk 'BEGIN {print "listeria:"}; (FS="\t") {if($9 ~/Listeria/ && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' QC_summary.txt
 
-awk 'BEGIN {print "gonorrhoeae:"}; (FS="\t") {if($10 == "Neisseria gonorrhoeae" && $2 >= 800000 && $16 <= 500 ) print "- " $1 }'  QC_summary.txt
+awk 'BEGIN {print "gonorrhoeae:"}; (FS="\t") {if($9 == "Neisseria gonorrhoeae" && $2 >= 800000 && $15 <= 500 ) print "- " $1 }'  QC_summary.txt
 
-awk 'BEGIN {print "spneumoniae:"}; (FS="\t") {if($0 ~/spneumoniae/ && $2 >= 800000 && $16 <= 500 ) print "- " $1 }' QC_summary.txt | sed 's,filtered_contigs/,,g' | cut -f 1 -d "." 
+awk 'BEGIN {print "spneumoniae:"}; (FS="\t") {if($0 ~/spneumoniae/ && $2 >= 800000 && $15 <= 500 ) print "- " $1 }' QC_summary.txt | sed 's,filtered_contigs/,,g' | cut -f 1 -d "." 
 
-awk 'BEGIN {print "spyogenes:"}; (FS="\t") {if($10 == "Streptococcus pyogenes" && $2 >= 800000 && $16 <= 500 ) print "- " $1 }' QC_summary.txt
+awk 'BEGIN {print "spyogenes:"}; (FS="\t") {if($9 == "Streptococcus pyogenes" && $2 >= 800000 && $15 <= 500 ) print "- " $1 }' QC_summary.txt
 
-awk 'BEGIN {print "shigella:"}; (FS="\t") {if($10 ~/Shigella/ || $12 ~/Shigella/ && $2 >= 1000000 && $16 <= 500 ) print "- " $1  }' QC_summary.txt
+awk 'BEGIN {print "shigella:"}; (FS="\t") {if($9 ~/Shigella/ || $11 ~/Shigella/ && $2 >= 1000000 && $15 <= 500 ) print "- " $1  }' QC_summary.txt
 
-awk 'BEGIN {print "Ssonnei:"}; (FS="\t") {if($10 == "Shigella sonnei" && $2 >= 1000000 && $16 <= 500 || $12 == "Shigella sonnei" && $2 >= 1000000 && $16 <= 500 ) print "- " $1  }' QC_summary.txt
+awk 'BEGIN {print "Ssonnei:"}; (FS="\t") {if($9 == "Shigella sonnei" && $2 >= 1000000 && $15 <= 500 || $11 == "Shigella sonnei" && $2 >= 1000000 && $15 <= 500 ) print "- " $1  }' QC_summary.txt
 
-awk 'BEGIN {print "ecoli:"}; (FS="\t") {if($10 == "Escherichia coli" && $12 !~/Shigella/ && $2 >= 1000000 && $16 <= 500 ) print "- " $1 }' QC_summary.txt
+awk 'BEGIN {print "ecoli:"}; (FS="\t") {if($9 == "Escherichia coli" && $11 !~/Shigella/ && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' QC_summary.txt
 
-awk 'BEGIN {print "meningitidis:"}; (FS="\t") {if($10 == "Neisseria meningitidis" && $2 >= 800000 && $16 <= 500 ) print "- " $1 }' QC_summary.txt
+awk 'BEGIN {print "meningitidis:"}; (FS="\t") {if($9 == "Neisseria meningitidis" && $2 >= 800000 && $15 <= 500 ) print "- " $1 }' QC_summary.txt
 
-awk 'BEGIN {print "tuberculosis:"}; (FS="\t") {if($10 == "Mycobacterium tuberculosis" && $2 >= 1000000 && $16 <= 500 ) print "- " $1 }' QC_summary.txt 
+awk 'BEGIN {print "tuberculosis:"}; (FS="\t") {if($9 == "Mycobacterium tuberculosis" && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' QC_summary.txt 
 
-awk 'BEGIN {print "legionella:"}; (FS="\t") {if($10 ~/Legionella/ && $2 >= 1000000 && $16 <= 500 ) print "- " $1 }' QC_summary.txt
+awk 'BEGIN {print "legionella:"}; (FS="\t") {if($9 ~/Legionella/ && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' QC_summary.txt
+
+awk 'BEGIN {print "paeruginosa:"}; (FS="\t") {if($9 == "Pseudomonas aeruginosa" && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' QC_summary.txt
 
 
 
