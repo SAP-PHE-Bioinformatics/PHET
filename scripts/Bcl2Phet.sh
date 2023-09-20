5ed42a53e48dc700c3c5f9c264bb8d6e941350bf
Kaur <fhiakau@frgeneseq03.had.sa.gov.au>
2023-09-20 16:28:54 +0930

    correct script

--- scripts/Bcl2Phet.sh
+++ scripts/Bcl2Phet.sh
@@ -6,29 +6,29 @@ dir=$( dirname $input)
 
 ### Load module
 
-#source /phe/tools/miniconda3/etc/profile.d/conda.sh
+source /phe/tools/miniconda3/etc/profile.d/conda.sh
 
-#conda activate phesiqcal
+conda activate phesiqcal
 
 ### Running bcl2fastq
 
-#BCL=$(sbatch --job-name bcl2fastq --mem 100G --ntasks 32 --time 960:00:00 -D /phe/tools/phesiqcal/log/ --wrap "bcl2fastq --sample-sheet $input --runfolder-dir $dir --no-lane-splitting --ignore-missing-bcls --ignore-missing-filter --ignore-missing-positions --output-dir $dir/BaseCalls")
+BCL=$(sbatch --job-name bcl2fastq --mem 100G --ntasks 32 --time 960:00:00 -D /phe/tools/phesiqcal/log/ --wrap "bcl2fastq --sample-sheet $input --runfolder-dir $dir --no-lane-splitting --ignore-missing-bcls --ignore-missing-filter --ignore-missing-positions --output-dir $dir/BaseCalls")
 
 ### Create folder
 folder=$(awk -F ',' 'FNR == 4 {print $2}' $input)
 
-#mkdir -p /scratch/phesiqcal/$folder
+mkdir -p /scratch/phesiqcal/$folder
 
 cd /scratch/phesiqcal/$folder
 
 ### Creating config file
 
-#awk -F ',' 'BEGIN{ print "controls+samples:"}; {IGNORECASE=1}; FNR > 21 {if($2 !="" && $0 !~/Metagenomic/ && $0 !~/FUNGAL/) print "- " $2|"sort -u"}' $input > config.yaml
-#awk -F ',' 'BEGIN{ print "samples:"}; {IGNORECASE=1}; FNR > 21 {if($2 !="" && $0 !~/NEG/ && $0 !~/Metagenomic/ && $0 !~/FUNGAL/) print "- " $2|"sort -u"}' $input >> config.yaml
+awk -F ',' 'BEGIN{ print "controls+samples:"}; {IGNORECASE=1}; FNR > 21 {if($2 !="" && $0 !~/Metagenomic/ && $0 !~/FUNGAL/) print "- " $2|"sort -u"}' $input > config.yaml
+awk -F ',' 'BEGIN{ print "samples:"}; {IGNORECASE=1}; FNR > 21 {if($2 !="" && $0 !~/NEG/ && $0 !~/Metagenomic/ && $0 !~/FUNGAL/) print "- " $2|"sort -u"}' $input >> config.yaml
 
 ### Create input folder
 
-#mkdir -p /scratch/phesiqcal/$folder/input
+mkdir -p /scratch/phesiqcal/$folder/input
 
 cd /scratch/phesiqcal/$folder/input
 
@@ -43,11 +43,11 @@ done
 
 ### Create symlinks
 
-#for i in `ls $dir/BaseCalls/$folder/*.fastq.gz | cut -f 8 -d "/" | cut -f 1 -d "_" | sort -u`
-#do 
-#	ln -fs $dir/BaseCalls/$folder/"$i"_*R1_001.fastq.gz /scratch/phesiqcal/$folder/input/"$i"_R1.fastq.gz
-#  ln -fs $dir/BaseCalls/$folder/"$i"_*R2_001.fastq.gz /scratch/phesiqcal/$folder/input/"$i"_R2.fastq.gz
-#done
+for i in `ls $dir/BaseCalls/$folder/*.fastq.gz | cut -f 8 -d "/" | cut -f 1 -d "_" | sort -u`
+do 
+	ln -fs $dir/BaseCalls/$folder/"$i"_*R1_001.fastq.gz /scratch/phesiqcal/$folder/input/"$i"_R1.fastq.gz
+  ln -fs $dir/BaseCalls/$folder/"$i"_*R2_001.fastq.gz /scratch/phesiqcal/$folder/input/"$i"_R2.fastq.gz
+done
  
 ### Load phesiqcal module
 
@@ -57,16 +57,14 @@ conda activate phesiqcal
 
 ## Identify job_id of bcl2fastq on slurm
 
-#array=(${BCL// / })
-#JOBID_BCL=${array[3]}
+array=(${BCL// / })
+JOBID_BCL=${array[3]}
 
 ### Running QC for Negative controls via  - no longer required, included in main snakefile 16/06/2023. AK
 
 
 ### Running phesiqcal on slurm
-#phesiqcal=$(sbatch --dependency=afterok:${JOBID_BCL} --job-name phesiqcal -o slurm-%x-%j.out --mem 100G --ntasks 32 --time 960:00:00 -D /scratch/phesiqcal/$folder/ --wrap "snakemake -j 32 --configfile /scratch/phesiqcal/$folder/config.yaml --snakefile /phe/tools/PHET/scripts/Snakefile_phesiqcal --use-conda")
-
-phesiqcal=$(sbatch --job-name phesiqcal -o slurm-%x-%j.out --mem 100G --ntasks 32 --time 960:00:00 -D /scratch/phesiqcal/$folder/ --wrap "snakemake -j 32 --configfile /scratch/phesiqcal/$folder/config.yaml --snakefile /phe/tools/PHET/scripts/Snakefile_phesiqcal --use-conda")
+phesiqcal=$(sbatch --dependency=afterok:${JOBID_BCL} --job-name phesiqcal -o slurm-%x-%j.out --mem 100G --ntasks 32 --time 960:00:00 -D /scratch/phesiqcal/$folder/ --wrap "snakemake -j 32 --configfile /scratch/phesiqcal/$folder/config.yaml --snakefile /phe/tools/PHET/scripts/Snakefile_phesiqcal --use-conda")
 
 # Identifying job_id of phesiqcal on slurm
 array=(${phesiqcal// / })
