887345d93a5f1b73e580d4c92450b3db148be0e5
Kaur <fhiakau@frgeneseq03.had.sa.gov.au>
2024-04-04 15:37:12 +1030

    bug fixed for symlinks, and server specified

--- scripts/Fastq2Phet.sh
+++ scripts/Fastq2Phet.sh
@@ -24,19 +24,19 @@ cd /scratch/phesiqcal/$folder/
 ls $input | cut -f 1 -d "_" |
 while read line;
 do
-   ln -fs $input"$line"_*1*.gz /scratch/phesiqcal/$folder/input/"$line"_R1.fastq.gz
-   ln -fs $input"$line"_*2*.gz /scratch/phesiqcal/$folder/input/"$line"_R2.fastq.gz;
+   ln -fs $input"$line"*1*.gz /scratch/phesiqcal/$folder/input/"$line"_R1.fastq.gz
+   ln -fs $input"$line"*2*.gz /scratch/phesiqcal/$folder/input/"$line"_R2.fastq.gz;
 done
 )  2> symlink_error_log.txt
 
-(
-ls $input | cut -f 1 -d "_" |
-while read line;
-do
-   ln -fs $input"$line"_*R1*.gz /scratch/phesiqcal/$folder/input/"$line"_R1.fastq.gz
-   ln -fs $input"$line"_*R2*.gz /scratch/phesiqcal/$folder/input/"$line"_R2.fastq.gz;
-done
-)  2> symlink_error_log.txt
+# (
+# ls $input | cut -f 1 -d "_" |
+# while read line;
+# do
+#    ln -fs $input"$line"_*1*.gz /scratch/phesiqcal/$folder/input/"$line"_R1.fastq.gz
+#    ln -fs $input"$line"_*2*.gz /scratch/phesiqcal/$folder/input/"$line"_R2.fastq.gz;
+# done
+# )  2> symlink_error_log.txt
 
 
 cd /scratch/phesiqcal/$folder/ 
@@ -70,7 +70,7 @@ conda activate phesiqcal
 
 
 ### Running phesiqcal on slurm
-phesiqcal=$(sbatch --job-name phesiqcal --mem 100G --ntasks 32 --time 960:00:00 -D /scratch/phesiqcal/$folder/ --wrap "snakemake -j 32 --configfile /scratch/phesiqcal/$folder/config.yaml --snakefile /phe/tools/PHET/scripts/Snakefile_phesiqcal --use-conda")
+phesiqcal=$(sbatch --job-name phesiqcal --mem 100G --ntasks 32 --time 960:00:00 -w frgeneseq03 -D /scratch/phesiqcal/$folder/ --wrap "snakemake -j 32 --configfile /scratch/phesiqcal/$folder/config.yaml --snakefile /phe/tools/PHET/scripts/Snakefile_phesiqcal --use-conda")
 
 
 # Identifying job_id of phesiqcal on slurm
@@ -82,4 +82,4 @@ JOBID_phesiqcal=${array[3]}
 source /phe/tools/PHET/scripts/Start_TypingTools.sh
 
 # Running shell script to record versions of QC and typing tools + Databases in PHET.
-source /phe/tools/PHET/scripts/VersionRecord_PHETools.sh >> /scratch/phesiqcal/$folder/PHETools_Versions_$folder.txt &
+source /phe/tools/PHET/scripts/VersionRecord_PHETools.sh >> /scratch/phesiqcal/$folder/PHETools_Versions_$folder.csv &
