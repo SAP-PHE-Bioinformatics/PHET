f613690ec0cffa6b831f7495d0b1783cdd97ed13
Kaur <fhiakau@frgeneseq03.had.sa.gov.au>
2024-04-04 15:37:54 +1030

    removed old symlink_runner, server specified

--- scripts/Phesiqcal2Phet.sh
+++ scripts/Phesiqcal2Phet.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+## This script to run if pipeline crashed after completion of the basecalling process. 
+# To avoid repeating the basecalling process, run this script, overrides the existing output directories and results if the pipeline previously crashed. 
+
+# requires the full path to the MID output folder in scratch as input. 
+
+
 input=${1?Error:no path to MID folder given}
 
 folder=$(basename $input)
@@ -11,7 +17,7 @@ source /phe/tools/miniconda3/etc/profile.d/conda.sh
 conda activate phesiqcal
 
 ### Running phesiqcal on slurm
-phesiqcal=$(sbatch --job-name phesiqcal -o slurm-%x-%j.out --mem 100G --ntasks 32 --time 960:00:00 -D /scratch/phesiqcal/$folder/ --wrap "snakemake -j 32 --configfile /scratch/phesiqcal/$folder/config.yaml --snakefile /phe/tools/PHET/scripts/Snakefile_phesiqcal --use-conda")
+phesiqcal=$(sbatch -w frgeneseq03 --job-name phesiqcal -o slurm-%x-%j.out --mem 100G --ntasks 64 --time 960:00:00 -D /scratch/phesiqcal/$folder/ --wrap "snakemake -j 32 --configfile /scratch/phesiqcal/$folder/config.yaml --snakefile /phe/tools/PHET/scripts/Snakefile_phesiqcal --use-conda")
 
 # Identifying job_id of phesiqcal on slurm
 array=(${phesiqcal// / })
@@ -23,7 +29,6 @@ source /phe/tools/PHET/scripts/Start_TypingTools.sh
 # Running shell script to record versions of QC and typing tools + Databases in PHET.
 source /phe/tools/PHET/scripts/VersionRecord_PHETools.sh >> $input/PHETools_Versions_$folder.csv &
 
-# path to the shell script to create symlinks of Fastq files to selected pathogen folders
-source /phe/tools/PHET/scripts/Symlinks_runner.sh &
+# removed the symlink runner, snippy_runner gets executed instead through typing tools. 
 
 
