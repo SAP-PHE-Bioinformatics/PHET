83b856eba0eab32805e566f46ce1a885a262b4f5
Kaur <fhiakau@frgeneseq03.had.sa.gov.au>
2024-10-11 10:15:53 +1030

    Input arguments updated, one more organism added

--- scripts/phet_makefile.sh
+++ scripts/phet_makefile.sh
@@ -4,6 +4,17 @@
 QC_FILE=$1
 RUN_ID=$2
 OUTPUT_FILE=$3
+
+input=${1?ERROR! : no QC_summary.txt or RunID given}
+input=${2?ERROR! : no RunID given
+
+Please provide two inputs in this order: QC_summary.txt and RunID 
+
+Example usage:
+
+    /phe/tools/PHET/scripts/phet_makefile.sh QC_summary.txt MIDYYXXX
+}
+
  
 echo "RunID: $RUN_ID"
 
@@ -27,10 +38,15 @@ awk 'BEGIN {print "meningitidis:"}; (FS="\t") {if($9 == "Neisseria meningitidis"
 
 awk 'BEGIN {print "tuberculosis:"}; (FS="\t") {if($9 == "Mycobacterium tuberculosis" && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE" 
 
+awk 'BEGIN {print "m_abscessus:"}; (FS="\t") {if($9 ~/abscessus/ && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE" 
+
+awk 'BEGIN {print "m_intracellulare:"}; (FS="\t") {if($9 ~/intracellulare/ && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE" 
+
 awk 'BEGIN {print "legionella:"}; (FS="\t") {if($9 ~/Legionella/ && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE"
 
 awk 'BEGIN {print "paeruginosa:"}; (FS="\t") {if($9 == "Pseudomonas aeruginosa" && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE"
 
 awk 'BEGIN {print "saureus:"}; (FS="\t") {if($9 == "Staphylococcus aureus" && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE"
 
+awk 'BEGIN {print "hinfluenzae:"}; (FS="\t") {if($9 == "Haemophilus influenzae" && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE"
 
