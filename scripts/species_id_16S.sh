0fd8987efbd84a2b148e5b46dc7fc9750b1893c9
Kaur <fhiakau@frgeneseq03.had.sa.gov.au>
2026-03-31 11:35:25 +1030

    Automated in phet_runner

--- scripts/species_id_16S.sh
+++ scripts/species_id_16S.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-input=${1?Error:no folder to the sample folder within a phesiqcal output given}
+# runner version
+VERSION="v20260324"
+
+# Check for -v or --version BEFORE processing the input
+if [[ "$1" == "-v" || "$1" == "--version" ]]; then
+    echo "species_id_16S.sh: $VERSION"
+    exit 0
+fi
+
+input=${1?Error:no path to the sample folder within a phesiqcal output given}
 sample=$(basename $input)
+mid_dir=$(dirname $input)
 
 ### Load module
 
@@ -12,9 +22,10 @@ conda activate phesiqcal
 ### Create folder
 #folder=$(awk -F ',' 'FNR == 4 {print $2}' $input)
 
-mkdir -p $input/16S_hsp65_rpoB
+mkdir -p $mid_dir/PHET/16S_hsp65_rpoB/$sample
+outdir="$mid_dir/PHET/16S_hsp65_rpoB/$sample"
 
-cd $input
+# cd $input
 
 ## copy the taxonomy file to the folder (clean-up will delete this afterwards)
 export BLASTDB="/scratch/databases/16S_ncbi/"
@@ -23,17 +34,17 @@ export BLASTDB="/scratch/databases/16S_ncbi/"
 # Running gene identification on the main terminal as it is low intensity 
 
 ## extract each of the genes 
-awk 'BEGIN {RS=">"} /16S ribosomal RNA/ {print ">"$0}' prokka/$sample.ffn > 16S_hsp65_rpoB/"$sample"_16S.fasta
+awk 'BEGIN {RS=">"} /16S ribosomal RNA/ {print ">"$0}' $input/prokka/$sample.ffn > $outdir/"$sample"_16S.fasta
 #awk -v sq="'" 'BEGIN {RS=">"} /DNA-directed RNA polymerase subunit beta/ && !/DNA-directed RNA polymerase subunit beta$sq/ {print ">"$0}' prokka/$sample.ffn > "$sample"_rpoB.fasta
-awk 'BEGIN {RS=">"} /DNA-directed RNA polymerase subunit beta/ {print ">"$0}' prokka/$sample.ffn > 16S_hsp65_rpoB/"$sample"_rpoB.fasta
-awk 'BEGIN {RS=">"} /60 kDa chaperonin/ {print ">"$0}' prokka/$sample.ffn > 16S_hsp65_rpoB/"$sample"_hsp65.fasta
+awk 'BEGIN {RS=">"} /DNA-directed RNA polymerase subunit beta/ {print ">"$0}' $input/prokka/$sample.ffn > $outdir/"$sample"_rpoB.fasta
+awk 'BEGIN {RS=">"} /60 kDa chaperonin/ {print ">"$0}' $input/prokka/$sample.ffn > $outdir/"$sample"_hsp65.fasta
 
 
 ## run the 16S gene against the 16S local database and then format it correctly for reporting 
-blastn -db /scratch/databases/16S_ncbi/16S_ribosomal_RNA -query 16S_hsp65_rpoB/"$sample"_16S.fasta -out 16S_hsp65_rpoB/"$sample"_16S_blast.tab -outfmt "6 delim=, qseqid sacc  scomnames  qcovs nident length pident"
-sort -t',' -k7nr -k4nr -k6nr 16S_hsp65_rpoB/"$sample"_16S_blast.tab -o 16S_hsp65_rpoB/"$sample"_16S_blast.tab
-sed -i 's/,/\t/g' 16S_hsp65_rpoB/"$sample"_16S_blast.tab
-sed -i '1iAccession\tBlastMatchID\tBlastMatchSciName\tQueryCov\tnum_ident\tBLASTMatchLen\tpct_ident' 16S_hsp65_rpoB/"$sample"_16S_blast.tab
+blastn -db /scratch/databases/16S_ncbi/16S_ribosomal_RNA -query $outdir/"$sample"_16S.fasta -out $outdir/"$sample"_16S_blast.tab -outfmt "6 delim=, qseqid sacc  scomnames  qcovs nident length pident"
+sort -t',' -k7nr -k4nr -k6nr $outdir/"$sample"_16S_blast.tab -o $outdir/"$sample"_16S_blast.tab
+sed -i 's/,/\t/g' $outdir/"$sample"_16S_blast.tab
+sed -i '1iAccession\tBlastMatchID\tBlastMatchSciName\tQueryCov\tnum_ident\tBLASTMatchLen\tpct_ident' $outdir/"$sample"_16S_blast.tab
 
 
 
