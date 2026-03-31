befaebb89dc23236520dba6277b19b9411a820b0
Kaur <fhiakau@frgeneseq03.had.sa.gov.au>
2026-03-31 12:08:55 +1030

    minor fix to write seqID as strings in yaml file

--- scripts/phet_makefile.sh
+++ scripts/phet_makefile.sh
@@ -18,40 +18,43 @@ Example usage:
  
 echo "RunID: $RUN_ID"
 
-awk 'BEGIN {print "salmonella:"}; (FS="\t") {if($9 == "Salmonella enterica" && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE"
+awk 'BEGIN {print "salmonella:"}; (FS="\t") {if($9 == "Salmonella enterica" && $2 >= 1000000 && $15 <= 500 ) print "- \"" $1 "\"" }' "$QC_FILE"
 
-awk 'BEGIN {print "listeria:"}; (FS="\t") {if($9 ~/Listeria/ && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE"
+awk 'BEGIN {print "listeria:"}; (FS="\t") {if($9 ~/Listeria/ && $2 >= 1000000 && $15 <= 500 ) print "- \"" $1 "\"" }' "$QC_FILE"
 
-awk 'BEGIN {print "gonorrhoeae:"}; (FS="\t") {if($9 == "Neisseria gonorrhoeae" && $2 >= 800000 && $15 <= 500 ) print "- " $1 }'  "$QC_FILE"
+awk 'BEGIN {print "gonorrhoeae:"}; (FS="\t") {if($9 == "Neisseria gonorrhoeae" && $2 >= 800000 && $15 <= 500 ) print "- \"" $1 "\"" }'  "$QC_FILE"
 
-awk 'BEGIN {print "spneumoniae:"}; (FS="\t") {if($0 ~/spneumoniae/ && $2 >= 800000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE" | sed 's,filtered_contigs/,,g' | cut -f 1 -d "." 
+awk 'BEGIN {print "spneumoniae:"}; (FS="\t") {if($0 ~/spneumoniae/ && $2 >= 800000 && $15 <= 500 ) print "- \"" $1 "\"" }' "$QC_FILE" | sed 's,filtered_contigs/,,g' | cut -f 1 -d "." 
 
-awk 'BEGIN {print "spyogenes:"}; (FS="\t") {if($9 == "Streptococcus pyogenes" && $2 >= 800000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE"
+awk 'BEGIN {print "spyogenes:"}; (FS="\t") {if($9 == "Streptococcus pyogenes" && $2 >= 800000 && $15 <= 500 ) print "- \"" $1 "\"" }' "$QC_FILE"
 
-awk 'BEGIN {print "shigella:"}; (FS="\t") {if($9 ~/Shigella/ && $9 !~/Shigella sonnei/ && $2 >= 1000000 && $15 <= 500 || $11 ~/Shigella/ && $11 !~/Shigella sonnei/ && $2 >= 1000000 && $15 <= 500 ) print "- " $1  }' "$QC_FILE"
+awk 'BEGIN {print "shigella:"}; (FS="\t") {if($9 ~/Shigella/ && $9 !~/Shigella sonnei/ && $2 >= 1000000 && $15 <= 500 || $11 ~/Shigella/ && $11 !~/Shigella sonnei/ && $2 >= 1000000 && $15 <= 500 ) print "- \"" $1 "\""  }' "$QC_FILE"
 
-awk 'BEGIN {print "Ssonnei:"}; (FS="\t") {if($9 == "Shigella sonnei" && $2 >= 1000000 && $15 <= 500 || $11 == "Shigella sonnei" && $2 >= 1000000 && $15 <= 500 ) print "- " $1  }' "$QC_FILE"
+awk 'BEGIN {print "Ssonnei:"}; (FS="\t") {if($9 == "Shigella sonnei" && $2 >= 1000000 && $15 <= 500 || $11 == "Shigella sonnei" && $2 >= 1000000 && $15 <= 500 ) print "- \"" $1 "\""  }' "$QC_FILE"
 
-awk 'BEGIN {print "ecoli:"}; (FS="\t") {if($9 == "Escherichia coli" && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE"
+awk 'BEGIN {print "ecoli:"}; (FS="\t") {if($9 == "Escherichia coli" && $2 >= 1000000 && $15 <= 500 ) print "- \"" $1 "\"" }' "$QC_FILE"
 
-awk 'BEGIN {print "meningitidis:"}; (FS="\t") {if($9 == "Neisseria meningitidis" && $2 >= 800000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE"
+awk 'BEGIN {print "meningitidis:"}; (FS="\t") {if($9 == "Neisseria meningitidis" && $2 >= 800000 && $15 <= 500 ) print "- \"" $1 "\"" }' "$QC_FILE"
 
-awk 'BEGIN {print "tuberculosis:"}; (FS="\t") {if($9 == "Mycobacterium tuberculosis" && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE" 
+awk 'BEGIN {print "tuberculosis:"}; (FS="\t") {if($9 == "Mycobacterium tuberculosis" && $2 >= 1000000 && $15 <= 500 ) print "- \"" $1 "\"" }' "$QC_FILE" 
 
-awk 'BEGIN {print "m_abscessus:"}; (FS="\t") {if($9 ~/abscessus/ && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE" 
+awk 'BEGIN {print "m_abscessus:"}; (FS="\t") {if($9 ~/abscessus/ && $2 >= 1000000 && $15 <= 500 ) print "- \"" $1 "\"" }' "$QC_FILE" 
 
-awk 'BEGIN {print "m_intracellulare:"}; (FS="\t") {if($9 ~/intracellulare/ && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE" 
+awk 'BEGIN {print "m_intracellulare:"}; (FS="\t") {if($9 ~/intracellulare/ && $2 >= 1000000 && $15 <= 500 ) print "- \"" $1 "\"" }' "$QC_FILE" 
 
-awk 'BEGIN {print "legionella:"}; (FS="\t") {if($9 ~/Legionella/ && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE"
+awk 'BEGIN {print "legionella:"}; (FS="\t") {if($9 ~/Legionella/ && $2 >= 1000000 && $15 <= 500 ) print "- \"" $1 "\"" }' "$QC_FILE"
 
-awk 'BEGIN {print "paeruginosa:"}; (FS="\t") {if($9 == "Pseudomonas aeruginosa" && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE"
+awk 'BEGIN {print "paeruginosa:"}; (FS="\t") {if($9 == "Pseudomonas aeruginosa" && $2 >= 1000000 && $15 <= 500 ) print "- \"" $1 "\"" }' "$QC_FILE"
 
-awk 'BEGIN {print "saureus:"}; (FS="\t") {if($9 == "Staphylococcus aureus" && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE"
+awk 'BEGIN {print "saureus:"}; (FS="\t") {if($9 == "Staphylococcus aureus" && $2 >= 1000000 && $15 <= 500 ) print "- \"" $1 "\"" }' "$QC_FILE"
 
-awk 'BEGIN {print "hinfluenzae:"}; (FS="\t") {if($9 == "Haemophilus influenzae" && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE"
+awk 'BEGIN {print "hinfluenzae:"}; (FS="\t") {if($9 == "Haemophilus influenzae" && $2 >= 1000000 && $15 <= 500 ) print "- \"" $1 "\"" }' "$QC_FILE"
 
-awk 'BEGIN {print "Sagalactiae:"}; (FS="\t") {if($9 == "Streptococcus agalactiae" && $2 >= 800000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE"
+awk 'BEGIN {print "Sagalactiae:"}; (FS="\t") {if($9 == "Streptococcus agalactiae" && $2 >= 800000 && $15 <= 500 ) print "- \"" $1 "\"" }' "$QC_FILE"
 
-awk 'BEGIN {print "Vcholerae:"}; (FS="\t") {if($9 == "Vibrio cholerae" && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE"
+awk 'BEGIN {print "Vcholerae:"}; (FS="\t") {if($9 == "Vibrio cholerae" && $2 >= 1000000 && $15 <= 500 ) print "- \"" $1 "\"" }' "$QC_FILE"
 
-awk 'BEGIN {print "Klebsiella:"}; (FS="\t") {if($9 ~/Klebsiella/ && $2 >= 1000000 && $15 <= 500 ) print "- " $1 }' "$QC_FILE"
+awk 'BEGIN {print "Klebsiella:"}; (FS="\t") {if($9 ~/Klebsiella/ && $2 >= 1000000 && $15 <= 500 ) print "- \"" $1 "\"" }' "$QC_FILE"
+
+# 18/03/26 - AK - Selecting NTMs and Nocardia for 16s_rpoB_hsp analysis.
+awk 'BEGIN {print "16S_hsp65_rpoB:"}; (FS="\t") {if($9 ~/Myco/ && $9 !~/abscessus/ && $9 !~/tuberculosis/ && $9 !~/intracellulare/ || $9 ~/Nocardia/) print "- \"" $1 "\"" }' "$QC_FILE"
